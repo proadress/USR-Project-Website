0b7c7581ed29c135e028b8cff83d8f4f5015d656
cloud <yc359032@gmail.com>
2024-04-29 18:20:47 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
 import Sidebar from "@/components/Sidebar";
 import { HomeSlider, HotRowCard, NewRowCard } from "@/components/home";
+import { redirect } from "next/navigation";
 
 export default function Home() {
   const cardDatas: string[][] = [[], []];
-  return (
-    <>
-      <div className="flex">
-        <Sidebar planNum={5} itemNum={0} />
-        <div className="py-20 mx-auto w-2/3 space-y-10">
-          <HomeSlider />
-          <HotRowCard />
-          <NewRowCard />
-        </div>
-      </div>
-    </>
-  )
+  redirect("http://140.121.17.140:8080")
+  // return (
+  //   <>
+  //     <div className="flex">
+  //       <Sidebar planNum={5} itemNum={0} />
+  //       <div className="py-20 mx-auto w-2/3 space-y-10">
+  //         <HomeSlider />
+  //         <HotRowCard />
+  //         <NewRowCard />
+  //       </div>
+  //     </div>
+  //   </>
+  // )
 }
